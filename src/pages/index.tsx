1ae2d43bb3685ac46daa84b34e3fc3798d49f4e7
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-20 10:36:16 -0300

    wrap with tr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,12 +70,14 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
         <table cellSpacing={0}>
           <thead>
-            <th></th>
-            <th>Podcast</th>
-            <th>Integrantes</th>
-            <th>Data</th>
-            <th>Duração</th>
-            <th></th>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
           </thead>
 
           <tbody>
