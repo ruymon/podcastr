f1dd42741784fba650aaaf8969882029d60bd5e8
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-20 07:38:45 -0300

    Remove unused types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import { GetStaticProps } from "next";
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
-  members: string;
-  published_at: string;
   thumbnail: string;
   description: string;
-  file: File;
-};
-
-type File = {
-  url: string;
-  type: string;
+  members: string;
   duration: number;
-}
+  durationString: string;
+  published_at: string;
+  url: string;
+};
 
 type HomeProps = {
   episodes: Episode[];
@@ -39,9 +39,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   });
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      thumbnail: episode.thumbnail,
+      description: episode.description,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy' , { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
