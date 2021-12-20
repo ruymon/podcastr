5fec98eaf0f5bc30796636cda65fa3d14c375142
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-20 15:57:33 -0300

    SSG paths config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -124,7 +124,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get('episodes', {
+  const { data } = await api.get('/episodes', {
     params: {
       _limit: 12,
       _sort: 'published_at',
