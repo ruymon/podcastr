4836627a8b51d573930d6ea5b77417b69865a363
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-26 15:53:20 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+
+
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+                    {/* TODO Add SEO */}
                 </Head>
                 <body>
                     <Main />
